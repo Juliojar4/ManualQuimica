40450d5c2ca10996ac3ac20b83a32f6065bfe480
julioJar4 <juliojaraitape@hotmail.com>
2025-07-25 00:32:13 -0300

    add my accont

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,11 +1,11 @@
-<header class="banner text-red-50">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
-  <h1 class="text-red-800">ola gente</h1>
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+<header class=" bg-blue-300 py-5 banner">
+  <div class="container flex justify-between items-center mx-auto">
+    {!! get_custom_logo() !!}
+    
+    @if (has_nav_menu('primary_navigation'))
+      <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+      </nav>
+    @endif
+  </div>
 </header>
